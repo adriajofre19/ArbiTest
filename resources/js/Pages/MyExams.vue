70827b8c7363ebec7794227b1d0ba461050bdab7
adriajofre19 <118483942+adriajofre19@users.noreply.github.com>
2024-06-05 20:01:12 +0200

    canvis

--- resources/js/Pages/MyExams.vue
+++ resources/js/Pages/MyExams.vue
@@ -12,10 +12,10 @@ const props = defineProps ({
 <template>
     <AuthenticatedLayout>
 
-    <div class="py-12" >
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 mb-4" v-for="exam in exams" :key="exam.id">
+    <div class="py-12">
+            <div class="max-w-7xl mx-auto px-6 lg:px-8 mb-4" v-for="exam in exams" :key="exam.id">
                 <a :href="'/exam-solutions/'+exam.id">
-                <div class="bg-white overflow-hidden flex justify-between shadow-sm sm:rounded-lg px-4 ">
+                <div class="bg-white overflow-hidden block md:flex justify-between shadow-sm rounded-lg px-4 ">
                     <div class="p-6 text-gray-900">{{ exam.name }}</div>
                     <div class="p-6 text-gray-900">{{ exam.correct_questions }} / {{ exam.total_questions }}</div>
                     <div class="p-6 text-gray-900">
